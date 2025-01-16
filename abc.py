5f492fb138b9a583a3658faa2be74bdd8f8970ad
tarangverma <tarangverma004@gmail.com>
2025-01-16 19:17:46 +0530

    new commit

---
+++ abc.py
@@ -0,0 +1,16 @@
+# a = {5,8,10}
+
+#     try :
+#         print(a[3])
+#     try :
+#         if a[3] > a[2]:
+#             print(a[3])
+#     except ZeroDivisionError :
+#         print("Cannot devide by zero")
+# except: IndexError :
+#     print("The mentioned index i not found")
+# finally :
+#     print("You are good")
+
+
+a = {4}
