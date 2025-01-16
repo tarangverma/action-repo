b71d499ab8d17f8f04d6e544085311ead343df4a
tarangverma <tarangverma004@gmail.com>
2025-01-16 21:25:05 +0530

    new update

--- abc.py
+++ abc.py
@@ -1,20 +1,5 @@
-# a = {5,8,10}
-
-#     try :
-#         print(a[3])
-#     try :
-#         if a[3] > a[2]:
-#             print(a[3])
-#     except ZeroDivisionError :
-#         print("Cannot devide by zero")
-# except: IndexError :
-#     print("The mentioned index i not found")
-# finally :
-#     print("You are good")
-
-
 a = {4. 5, 7}
-try:
-    print(a[3])
-    print(a[3] > a[2])
-    print('helo')
+
+print(a[3])
+print(a[3] > a[2])
+print('hello')
